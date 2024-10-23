aa828dccc6d3d2e89a8a285e045ffb48dc4e1d96
Timothy <timothyolofson@gmail.com>
2024-10-23 03:40:43 -0700

    99% Completed due to become stuck on finishing touchups in order to make it a 1 to 1 clone. Though, I coded well over 90% of it myself besides the super complecated stuff I just couldn't figure out on my own, nor have time to. I need to move onto JS asap. I'm already A month behind.

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ Timothy Olofson
 Frontend Simplified(FES)
 Final Project(Week2)
 */
+
+function openMenu() {
+  document.body.classList.add("menu--open");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu--open");
+}
